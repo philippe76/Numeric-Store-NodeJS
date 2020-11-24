6e475d4cfc4b386565f4e73bd5fc43b415de7e18
Philippe Lanougadere <soufiane92230@gmail.com>
2020-11-24 10:15:15 +0100

    Create request to watch URL

---
+++ README.md
@@ -0,0 +1,2 @@
+Creating a URL monitoring app with Node JS for a
+recruitment test
--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const request = require('request');
 
 const app = express();
 const port = 8080;
 
+
+
+
+const urlWatcher = (req, res) => {  
+  request('https://dev.fractal-it.fr:8443/fake_health_test?dynamic=true   ', (error, response) => {
+    if (!error && response.statusCode == 200) {
+        message = `status is ${response.statusMessage}`;
+        details = JSON.stringify(response, null, 2);
+    } else {
+        message = `Error : status is ${response.statusMessage}`;
+        details = JSON.stringify(response, null, 2);
+    }
+    console.log(message);
+    console.log(details);
+  })
+ 
+};
+
+
+
+
 app.get('/', (req, res) => {
-    res.status(200).json({message: 'OK'})
+    urlWatcher()
+    res.status(200).json({message: 'Watching URL'})
 })
+
 
 app.listen(port, () => console.log(`listenning on port ${port}`))
